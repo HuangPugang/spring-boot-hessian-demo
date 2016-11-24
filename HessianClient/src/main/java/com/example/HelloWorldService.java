dd1d8b91db93bb6bbfa54991801fe5ab8dc2faa3
Paul <huangpg@59store.com>
2016-11-24 17:24:11 +0800

    hessian demo first commit

---
+++ HessianClient/src/main/java/com/example/HelloWorldService.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * Created by paul on 16/11/24.
+ */
+public interface HelloWorldService {
+
+    String sayHello(String name);
+}
---
+++ HessianClient/src/main/java/com/example/HessianClientApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.caucho.HessianProxyFactoryBean;
+
+@SpringBootApplication
+public class HessianClientApplication {
+	@Bean
+	public HessianProxyFactoryBean helloClient() {
+		HessianProxyFactoryBean factory = new HessianProxyFactoryBean();
+		factory.setServiceUrl("http://localhost:8090/HelloWorldService");
+		factory.setServiceInterface(HelloWorldService.class);
+		return factory;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HessianClientApplication.class, args);
+	}
+}
